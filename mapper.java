bf3a39b9eef60d2ee75d82ff768a9a3413ff130e
Manish Raj <84199658+mani504@users.noreply.github.com>
2025-05-26 02:13:31 +0530

    Update and rename mapper to mapper.java

--- mapper.java
+++ mapper.java
@@ -65,4 +65,59 @@ public class FlowConfigMapper {
                 .createBy(entity.getCreateBy())
                 .build();
     }
+
+    public FlowConfigDataDTO toDataDTO(FlowConfig entity) {
+    if (entity == null) return null;
+
+    return FlowConfigDataDTO.builder()
+            .name(entity.getName())
+            .email(entity.getEmail())
+            .description(entity.getDescription())
+            .scheduling(entity.isScheduling())
+            .schedulingCron(entity.getSchedulingCron())
+            .createBy(entity.getCreateBy())
+            .targetDevices(entity.getTargetDevices())
+            .serviceVerificationBefore(toBeforeDTO(entity.getServiceVerificationBefore()))
+            .configSettings(toConfigSettingsDTO(entity.getConfigSettings()))
+            .serviceVerificationAfter(toAfterDTO(entity.getServiceVerificationAfter()))
+            .build();
+}
+
+private ServiceVerificationBeforeDTO toBeforeDTO(ServiceVerificationBefore entity) {
+    if (entity == null) return null;
+    return ServiceVerificationBeforeDTO.builder()
+            .configurationsBackup(entity.isConfigurationsBackup())
+            .serviceCheckCommands(entity.getServiceCheckCommands())
+            .build();
+}
+
+
+private ConfigSettingsDTO toConfigSettingsDTO(ConfigSettings entity) {
+    if (entity == null) return null;
+    return ConfigSettingsDTO.builder()
+            .switchConfiguration(entity.getSwitchConfiguration())
+            .uplinkRedundancyTest(entity.isUplinkRedundancyTest())
+            .rebootDevices(entity.isRebootDevices())
+            .build();
+}
+
+
+private ServiceVerificationAfterDTO toAfterDTO(ServiceVerificationAfter entity) {
+    if (entity == null) return null;
+    return ServiceVerificationAfterDTO.builder()
+            .serviceCheckCommands(entity.getServiceCheckCommands())
+            .normalDeterminationCriteria(
+                entity.getNormalDeterminationCriteria().stream()
+                    .map(n -> NormalDeterminationCriteriaDTO.builder()
+                            .criteria(n.getCriteria())
+                            .condition(n.getCondition())
+                            .build())
+                    .collect(Collectors.toList())
+            )
+            .build();
+}
+
+    
+
+    
 }
